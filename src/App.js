e19a37e8d3cf1bea513b9577efbf4daaec85299c
Musab Momin <musab.momin@kapso.in>
2022-08-21 16:05:38 +0530

    project is completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import Register from "./Register";
+import Table from "./Table";
+
 
 function App(){
+    const [formData, setFormData] = useState([])
     return (
         <main className="main">
-            <Register />
+            <Register setFormData = {setFormData} />
+            <Table formData={formData} />
         </main>
     )
 }
