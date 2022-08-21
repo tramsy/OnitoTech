5bd71e782be2d2b6794e42b5831a8eaec44c2f41
Musab Momin <musab.momin@kapso.in>
2022-08-21 14:51:41 +0530

    form functionality is done

--- src/Personal.jsx
+++ src/Personal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Personal = () => {
+const Personal = ({ formik, handleClear }) => {
   return (
     <>
       <h3 className="heading">Personal Details</h3>
@@ -9,18 +9,36 @@ const Personal = () => {
           <label>
             Name<sup className="sup">*</sup>
           </label>
-          <input type="text" name="fullname" placeholder="Enter Name"/>
+          <div className="frm__field">
+            <input 
+            type="text" 
+            name="fullname" 
+            placeholder="Enter Name" 
+            value={formik.values.fullname} 
+            onChange={formik.handleChange}
+            />
+            { formik.values.fullname && <button className="btn" onClick={(eve)=>handleClear(eve, 'fullname') }>X</button>}
+          </div>
         </div>
         <div className="frm__row frm__row--right">
           <label>
             Date Of Birth or Age<sup className="sup">*</sup>
           </label>
-          <input type="text" name="dob" placeholder="DD/MM/YYYY or Age in Years"/>
+          <div className="frm__field">
+            <input 
+            type="text" 
+            name="dob" 
+            placeholder="DD/MM/YYYY or Age in Years"
+            value={formik.values.dob} 
+            onChange={formik.handleChange}
+            />
+            { formik.values.dob && <button className="btn" onClick={(eve)=>handleClear(eve, 'dob') }>X</button>}
+          </div>
           <label>
             Sex<sup className="sup">*</sup>
           </label>
-          <select className="sel-opt">
-            <option selected disabled>
+          <select className="sel-opt" name="sex" value={formik.values.sex} onChange={formik.handleChange}>
+            <option disabled>
               Enter sex
             </option>
             <option value="male">Male</option>
@@ -30,21 +48,40 @@ const Personal = () => {
       </div>
       <div className="frm__sec">
         <div className="frm__row">
-          <label>Mobile</label>
-          <input type="text" name="mobile" style={{ width: "70%" }} placeholder="Enter Mobile"/>
+        <label>Mobile</label>
+          <div className="frm__field" style={{ width: "70%" }} >
+            <input 
+            type="text" 
+            name="mobile"
+            placeholder="Enter Mobile"
+            value={formik.values.mobile} 
+            onChange={formik.handleChange}
+            />
+            { formik.values.mobile && <button className="btn" onClick={(eve)=>handleClear(eve, 'mobile') }>X</button>}
+          </div>
+
         </div>
         <div className="frm__row frm__row--right">
           <label className="lbl">Govt Issued ID</label>
-          <select className="sel-opt">
-            <option selected disabled>
-              ID type
+          <select className="sel-opt" name="idtype"  value={formik.values.idtype} onChange={formik.handleChange}>
+            <option disabled>
+              ID Type
             </option>
             <option value="aadhar card">Aahdar</option>
             <option value="passport">Passport</option>
             <option value="voter id">Voter Id</option>
             <option value="pan card">PanCard</option>
           </select>
-          <input type="text" name="id" placeholder="Enter Govt ID" />
+          <div className="frm__field">
+            <input 
+            type="text" 
+            name="idnumber" 
+            placeholder="Enter Govt ID" 
+            value={formik.values.idnumber} 
+            onChange={formik.handleChange}
+            />
+            { formik.values.idnumber && <button className="btn" onClick={(eve)=>handleClear(eve, 'idnumber') }>X</button>}
+          </div>
         </div>
       </div>
     </>
